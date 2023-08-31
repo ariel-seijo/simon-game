e93620acbefea47245e069f3c870818ec71ce152
ariel <ariel.seijo2@gmail.com>
2023-08-31 11:56:06 -0300

    se agregan los sonidos nuevos a los botones

--- script.js
+++ script.js
@@ -1,42 +1,20 @@
-/*
 const redbutton = document.querySelector('#quarter-circle-1');
 const greenbutton = document.querySelector('#quarter-circle-2');
 const yellowbutton = document.querySelector('#quarter-circle-3');
 const bluebutton = document.querySelector('#quarter-circle-4');
-*/
-
-const boton1 = document.querySelector('#boton1');
-const boton2 = document.querySelector('#boton2')
-const boton3 = document.querySelector('#boton3')
-const boton4 = document.querySelector('#boton4')
 
 const redsound = new Audio();
 const greensound = new Audio();
 const yellowsound = new Audio();
 const bluesound = new Audio();
 
-redsound.src='./sounds/boton1.mp3';
+
+yellowsound.src='/sounds/boton2.mp3';
+greensound.src='/sounds/boton2.mp3';
+redsound.src='./sounds/boton2.mp3';
 bluesound.src='./sounds/boton2.mp3';
-yellowsound.src='/sounds/boton4.mp3';
-greensound.src='/sounds/boton3.mp3';
 
-boton1.addEventListener('click', ()=> {
-    redsound.play()
-});
-
-boton2.addEventListener('click', ()=> {
-    bluesound.play()
-});
-
-boton3.addEventListener('click', ()=> {
-    yellowsound.play()
-});
-
-boton4.addEventListener('click', ()=> {
-    greensound.play()
-});
-
-/* redbutton.addEventListener('click', ()=> {
+redbutton.addEventListener('click', ()=> {
     redbutton.style.backgroundColor = 'rgb(170, 0, 0)'
     redsound.play()
     setTimeout(()=> {
@@ -66,5 +44,5 @@ yellowbutton.addEventListener('click', ()=> {
     setTimeout(()=> {
         yellowbutton.style.backgroundColor = 'yellow'
     }, 100);
-}); */
+});
 
