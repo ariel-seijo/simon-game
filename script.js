eb98d623f286cd38660c2454047ddbf9454fae25
ariel <ariel.seijo2@gmail.com>
2023-09-05 16:22:39 -0300

    se agrega los sonidos de los botones

--- script.js
+++ script.js
@@ -8,78 +8,119 @@ const redsound = new Audio('redsound.wav');
 const greensound = new Audio('greensound.wav');
 const yellowsound = new Audio('yellowsound.wav');
 const bluesound = new Audio('bluesound.wav');
+const losesound = new Audio('lose.wav');
 
-function activatebutton(sound, button, color1, color2, time) {
-    sound.play()
-    button.style.backgroundColor = color1
-    setTimeout(() => {
-        button.style.backgroundColor = color2;
-    }, time);
-}
+let patronMaquina = [];
+let patronJugador = [];
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-const redactivated = [redsound, redbutton, 'red', 'rgb(201,27,28)', '100'];
-const greenactivated = [greensound, greenbutton, 'green', 'rgb(1,156,78)', '100']
-const blueactivated = [bluesound, bluebutton, 'rgb(0, 162, 255)', 'rgb(0,119,188)', '100']
-const yellowactivated = [yellowsound, yellowbutton, 'rgb(255, 217, 0)', 'rgb(218,189,25)', '100']
+iniciarboton.addEventListener('click', ()=> {
+    reiniciar();
+    ronda();
+});
 
-let presentacion = [redactivated, greenactivated, blueactivated, yellowactivated, blueactivated];
-let botones = [redactivated, greenactivated, blueactivated, yellowactivated]
-let juego = []
-let comenzar = false;
+quitarPermisoInput();
 
-let contador = 0
-iniciarboton.addEventListener('click', () => {
-    for (let n of presentacion) {
-        console.log(n)
-        contador += 200;
-        setTimeout(() => {
-            activatebutton(n[0], n[1], n[2], n[3], n[4]);
-        }, parseInt(contador));
-    } contador = 0; comenzar = true;
+function reiniciar() {
+    patronMaquina = [];
+    patronJugador = [];
+}
 
-    while (comenzar) {
-        juego.push(botones[getRandomInt(4)]);
+function obtenerBotonAleatorio() {
+    const $botones = document.querySelectorAll('.quarter-circle');
+    const indice = Math.floor(Math.random() * $botones.length);
+    return $botones[indice];
+}
 
-        setTimeout(() => {
-            console.log('XD')
-            for (let i of juego) {
-                contador += 200;
-                setTimeout(() => {
-                    activatebutton(i[0], i[1], i[2], i[3], i[4]);
-                }, parseInt(contador))
-            }; contador = 0;
-        }, '1000');
+function ronda() {
+    console.log('Turno de la máquina');
+    quitarPermisoInput();
 
-        if (juego.length > 5) {
-            juego.length = 0;
-            comenzar = false;
-        }
+    const $nuevoBotton = obtenerBotonAleatorio();
+    patronMaquina.push($nuevoBotton);
+    console.log(patronMaquina)
+
+    const RETRASO_TURNO_JUGADOR = ((patronMaquina.lenght + 1) * 1000);
+
+    patronMaquina.forEach(function($boton, index) {
+        const RETRASO_MS = (index + 1) * 500;
+        setTimeout(function() {
+            encenderBoton($boton);
+        }, RETRASO_MS);
+    })
+
+    setTimeout(function() {
+        console.log('es tu turno');
+        darPermisoInput(); 
+    }, RETRASO_TURNO_JUGADOR);
+
+    patronJugador = [];
+}
+
+function turnoMaquina(){
+    patronMaquina.forEach(($boton) => encenderBoton($boton));
+}
+
+function darPermisoInput(){
+    document.querySelectorAll('.quarter-circle').forEach(function($boton) {
+        $boton.onclick = inputDelUsuario;
+    })
+}
+
+function quitarPermisoInput(){
+    document.querySelectorAll('.quarter-circle').forEach(function($boton) {
+        $boton.onclick = function(){
+        };
+    })
+}
+
+function inputDelUsuario(e){
+    const $boton = e.target;
+    encenderBoton($boton);
+    patronJugador.push($boton);
+
+    const $botonMaquina = patronMaquina[patronJugador.length - 1];
+    if ($boton.id !== $botonMaquina.id) {
+        perder();
+        return;
     }
-});
 
+    if (patronJugador.length === patronMaquina.length) {
+        quitarPermisoInput();
+        setTimeout(() => {
+            ronda()
+        }, 500);
+    }
+}
 
+function encenderBoton($boton) {
+    $boton.style.opacity = 1;
+    switch ($boton.id) {
+        case 'quarter-circle-1':
+            redsound.play();
+            break;
+        case 'quarter-circle-2':
+            greensound.play();
+            break;
+        case 'quarter-circle-3':
+            yellowsound.play();
+            break;
+        case 'quarter-circle-4':
+            bluesound.play();
+            break;
+    }
+    setTimeout(()=> {
+        $boton.style.opacity = 0.6;
+    }, 200);
+}
 
-
-
-
-redbutton.addEventListener('click', () => {
-    activatebutton(redsound, redbutton, 'red', 'rgb(201,27,28)', '100');
-});
-
-greenbutton.addEventListener('click', () => {
-    activatebutton(greensound, greenbutton, 'green', 'rgb(1,156,78)', '100');
-});
-
-bluebutton.addEventListener('click', () => {
-    activatebutton(bluesound, bluebutton, 'rgb(0, 162, 255)', 'rgb(0,119,188)', '100');
-});
-
-yellowbutton.addEventListener('click', () => {
-    activatebutton(yellowsound, yellowbutton, 'rgb(255, 217, 0)', 'rgb(218,189,25)', '100');
-});
-
-
+function perder() {
+    quitarPermisoInput();
+    setTimeout(() => {
+        losesound.play();    
+    }, 500);
+    console.log('¡Perdiste!');
+}
